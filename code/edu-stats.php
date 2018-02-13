61c87b983af07f68567588332d8ecb0b8471c9e2
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2018-02-13 17:40:54 -0500

    Update edu-stats.php

--- code/edu-stats.php
+++ code/edu-stats.php
@@ -1,12 +1,12 @@
 <?php
 	//**************************************************
-	// Edu-Stats v.1.9.1
+	// Edu-Stats v.2.0.0
 	// Academic Statistics Parser
-	// Google Scholar, Scopus, ResearcherID
+	// Google Scholar, Scopus, ResearcherID, Mendeley
 	//
 	//
 	// Developed by Kaveh Bakhtiyari ( http://www.bakhtiyari.com )
-	// v1.9.1: 24 April 2017
+	// v2.0.0: 13 February 2018
 	//**************************************************
 	error_reporting(E_ERROR | E_PARSE);
 	//error_reporting(E_ALL);
@@ -16,15 +16,14 @@
 	//ini_set("log_errors", 1);
 	//ini_set("error_log", "php-error.log");
 	
-	ini_set("user_agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
+	ini_set("user_agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36");
 	ini_set('allow_url_fopen',1);
 
-	$EduStatsVersion = "1.9.1";
+	$EduStatsVersion = "2.0.0";
 
-	function get_web_page( $url, $cookiesIn = '', $param = '' ){
+	function get_web_page( $url, $post = false, $cookiesIn = '', $param = ''){
         $options = array(
-		CURLOPT_POST           => true,
-		CURLOPT_POSTFIELDS     => $param,   //Set POST parameters
+		CURLOPT_POST           => $post,
 		CURLOPT_RETURNTRANSFER => true,     // return web page
 		CURLOPT_HEADER         => true,     //return headers in addition to content
 		CURLOPT_FOLLOWLOCATION => true,     // follow redirects
@@ -36,9 +35,13 @@
 		CURLINFO_HEADER_OUT    => true,
 		CURLOPT_SSL_VERIFYPEER => false,     // Disabled SSL Cert checks
 		CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-		CURLOPT_USERAGENT      => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+		CURLOPT_USERAGENT      => "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36",
 		CURLOPT_COOKIE         => $cookiesIn
         );
+		
+		if (strlen($param) > 0){
+			$options[CURLOPT_POSTFIELDS] = $param; //Set POST parameters
+		}
 		
         $ch      = curl_init( $url );
         curl_setopt_array( $ch, $options );
@@ -68,10 +71,11 @@
 	$Gi10Index = -1;
 	
 	//Scopus Variables
-	$SDocuments = -1;
-	$ShIndex = -1;
-	$SCoAuthors = -1;
-	$SReferences = -1;
+	$Scopus_hIndex = -1;
+	$Scopus_Documents = -1;
+	$Scopus_CitationDocuments = -1;
+	$Scopus_CoAuthors = -1;
+
 	
 	//ResearcherID Variables
 	$ResearcherID_totalArticleCount = -1;
@@ -81,15 +85,25 @@
 	$ResearcherID_hindex = -1;
 	$ResearcherID_lastUpdatedString = -1;
 	
+	//Mendeley Variables
+	$Mendeley_Media = -1;
+	$Mendeley_hIndex = -1;
+	$Mendeley_Citations = -1;
+	$Mendeley_Readers = -1;
+	$Mendeley_Views = -1;
+
 	$GScholar = "";
-	$SScopusID = "";
+	$ScopusID = "";
 	$ReID = "";
+	$MendeleyID = "";
+
 	$prefix = "";
 	$output = "javascript";
 	
 	$GURL = "";
-	$SURL = "";
+	$ScopusURL = "";
 	$ReIDURL = "";
+	$MendeleyURL = "";
 	
 	
 	if(isset($_GET['google']) && !empty($_GET['google'])){
@@ -98,14 +112,19 @@
 		google($GURL);
 	}
 	if(isset($_GET['scopus']) && !empty($_GET['scopus'])){
-		$SScopusID = $_GET['scopus'];
-		$SURL = "https://www.scopus.com/authid/detail.uri?authorId=" . $SScopusID;
-		scopus($SURL);
+		$ScopusID = $_GET['scopus'];
+		$ScopusURL = "https://www.scopus.com/authid/detail.uri?authorId=" . $ScopusID;
+		scopus($ScopusURL);
 	}
 	if(isset($_GET['reid']) && !empty($_GET['reid'])){
 		$ReID = $_GET['reid'];
 		$ReIDURL = "http://www.researcherid.com/rid/" . $ReID;
 		researcherid($ReIDURL);
+	}
+	if(isset($_GET['mendeley']) && !empty($_GET['mendeley'])){
+		$MendeleyID = $_GET['mendeley'];
+		$MendeleyURL = "https://www.mendeley.com/profiles/" . $MendeleyID . "/stats/";
+		mendeley($MendeleyURL);
 	}
 	if(isset($_GET['prefix']) && !empty($_GET['prefix'])){
 		$prefix = $_GET['prefix'];
@@ -162,38 +181,23 @@
 	
 	function scopus($scopusurl){
 		
-		global $SDocuments;
-		global $ShIndex;
-		global $SCoAuthors;
-		global $SReferences;
+		global $Scopus_hIndex;
+		global $Scopus_Documents;
+		global $Scopus_CitationDocuments;
+		global $Scopus_CoAuthors;
+
+		$PageContent = get_web_page($scopusurl, false);
+		$Cookies = $PageContent['cookies'];
 		
-		$PageContent = get_web_page($scopusurl);
+		$PageContent = get_web_page($scopusurl, false, $Cookies);
 		$Code = $PageContent['content'];
-				
-		$SearchFROM = "'see all references cited by this author'";
-		$strTO = "</a>";
-		$dblFROM = strpos($Code,$SearchFROM,1);
-		$SearchFROM = ">";
-		$dblFROM = strpos($Code,$SearchFROM,$dblFROM);
-		$dblFROM = $dblFROM + strlen($SearchFROM);
-		$dblTO = strpos($Code,$strTO,$dblFROM);
-		$startPoint = $dblTO;
-		$dblTO = $dblTO - $dblFROM;
 		
-		$strResult = trim(substr($Code,$dblFROM,$dblTO));
-		if (strlen($strResult) > 0){
-			$strConverted = str_replace(",","",$strResult);
-			$SReferences = $strConverted;
-		}
+		$SearchFROM = "<section id=\"authorDetailsHindex\">";
+		$startPoint = strpos($Code,$SearchFROM,1);
 		
-		
-		$strStart = "\"addInfoRow row1\"";
-		$startPoint = strpos($Code,$strStart,1);
-		
-		$SearchFROM = " title='View a list of documents by this author below'>";
-		$strTO = "</a>";
+		$SearchFROM = "<span class=\"fontLarge\">";
+		$strTO = "</span>";
 		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
-		
 		$dblFROM = $dblFROM + strlen($SearchFROM);
 		$dblTO = strpos($Code,$strTO,$dblFROM);
 		$startPoint = $dblTO;
@@ -202,13 +206,45 @@
 		$strResult = trim(substr($Code,$dblFROM,$dblTO));
 		if (strlen($strResult) > 0){
 			$strConverted = str_replace(",","",$strResult);
-			$SDocuments = $strConverted;
+			$Scopus_hIndex = $strConverted;
 		}
 		
+		$SearchFROM = "<section id=\"authorDetailsDocumentsByAuthor\">";
+		$startPoint = strpos($Code,$SearchFROM,1);
 		
+		$SearchFROM = "<span class=\"fontLarge pull-left\">";
+		$strTO = "</span>";
+		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
+		$dblFROM = $dblFROM + strlen($SearchFROM);
+		$dblTO = strpos($Code,$strTO,$dblFROM);
+		$startPoint = $dblTO;
+		$dblTO = $dblTO - $dblFROM;
 		
-		$strStart = "\"addInfoRow row3\"";
-		$startPoint = strpos($Code,$strStart,$startPoint);
+		$strResult = trim(substr($Code,$dblFROM,$dblTO));
+		if (strlen($strResult) > 0){
+			$strConverted = str_replace(",","",$strResult);
+			$Scopus_Documents = $strConverted;
+		}
+		
+		$SearchFROM = "<section id=\"authorDetailsTotalCitations\">";
+		$startPoint = strpos($Code,$SearchFROM,1);
+		
+		$SearchFROM = "<span class=\"btnText\">";
+		$strTO = "</span>";
+		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
+		$dblFROM = $dblFROM + strlen($SearchFROM);
+		$dblTO = strpos($Code,$strTO,$dblFROM);
+		$startPoint = $dblTO;
+		$dblTO = $dblTO - $dblFROM;
+		
+		$strResult = trim(substr($Code,$dblFROM,$dblTO));
+		if (strlen($strResult) > 0){
+			$strConverted = str_replace(",","",$strResult);
+			$Scopus_CitationDocuments = $strConverted;
+		}
+				
+		$SearchFROM = "<a id=\"coAuthLi\" data-toggle=\"tab\"";
+		$startPoint = strpos($Code,$SearchFROM,1);
 		
 		$SearchFROM = "<span>";
 		$strTO = "</span>";
@@ -221,27 +257,9 @@
 		$strResult = trim(substr($Code,$dblFROM,$dblTO));
 		if (strlen($strResult) > 0){
 			$strConverted = str_replace(",","",$strResult);
-			$ShIndex = $strConverted;
+			$Scopus_CoAuthors = $strConverted;
 		}
-		
-		
-		$strStart = "\"addInfoRow row4\"";
-		$startPoint = strpos($Code,$strStart,$startPoint);
-		
-		$SearchFROM = " data-title='View a list of authors who have published with this author below'>";
-		$strTO = "</a>";
-		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
-		$dblFROM = $dblFROM + strlen($SearchFROM);
-		$dblTO = strpos($Code,$strTO,$dblFROM);
-		$startPoint = $dblTO;
-		$dblTO = $dblTO - $dblFROM;
-		
-		$strResult = trim(substr($Code,$dblFROM,$dblTO));
-		if (strlen($strResult) > 0){
-			$strConverted = str_replace(",","",$strResult);
-			$SCoAuthors = $strConverted;
-		}
-		
+
 	}
 	
 	function researcherid($ridurl){
@@ -271,7 +289,7 @@
 		$strCookies = "researcher.init=\"cmVzZWFyY2hlci5pbml0PVk=\"; JSESSIONID=4AC75E2B669DD47C9F49DE8485DF17B0";
 		$newURL = "http://www.researcherid.com/MetricsInPreviewPage.action";
 		
-		$PageContent = get_web_page($newURL,$strCookies,$param);
+		$PageContent = get_web_page($newURL, true, $strCookies, $param);
 		$Code = $PageContent['content'];
 		
 		
@@ -329,7 +347,90 @@
 		$ResearcherID_lastUpdatedString = trim(substr($Code,$dblFROM,$dblTO));
 		
 	}
-	
+
+	function mendeley($mendeleyurl){
+		
+		global $Mendeley_Media;
+		global $Mendeley_hIndex;
+		global $Mendeley_Citations;
+		global $Mendeley_Readers;
+		global $Mendeley_Views;
+		
+		$PageContent = get_web_page($mendeleyurl, false);
+		$Code = $PageContent['content'];
+
+		$SearchFROM = "Media mentions</span></h3></header><data class=\"number\" value=\"";
+		$strTO = "\">";
+		$dblFROM = strpos($Code,$SearchFROM,1);
+		$dblFROM = $dblFROM + strlen($SearchFROM);
+		$dblTO = strpos($Code,$strTO,$dblFROM);
+		$startPoint = $dblTO;
+		$dblTO = $dblTO - $dblFROM;
+		
+		$strResult = trim(substr($Code,$dblFROM,$dblTO));
+		if (strlen($strResult) > 0){
+			$strConverted = str_replace(",","",$strResult);
+			$Mendeley_Media = $strConverted;
+		}
+		
+		
+		$SearchFROM = "h-index</span></h3></header><data class=\"number\" value=\"";
+		$strTO = "\">";
+		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
+		$dblFROM = $dblFROM + strlen($SearchFROM);
+		$dblTO = strpos($Code,$strTO,$dblFROM);
+		$startPoint = $dblTO;
+		$dblTO = $dblTO - $dblFROM;
+		
+		$strResult = trim(substr($Code,$dblFROM,$dblTO));
+		if (strlen($strResult) > 0){
+			$strConverted = str_replace(",","",$strResult);
+			$Mendeley_hIndex = $strConverted;
+		}
+		
+		$SearchFROM = "Citations</span></h3></header><data class=\"number\" value=\"";
+		$strTO = "\">";
+		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
+		$dblFROM = $dblFROM + strlen($SearchFROM);
+		$dblTO = strpos($Code,$strTO,$dblFROM);
+		$startPoint = $dblTO;
+		$dblTO = $dblTO - $dblFROM;
+		
+		$strResult = trim(substr($Code,$dblFROM,$dblTO));
+		if (strlen($strResult) > 0){
+			$strConverted = str_replace(",","",$strResult);
+			$Mendeley_Citations = $strConverted;
+		}
+
+		$SearchFROM = "Readers</span></h3></header><data class=\"number\" value=\"";
+		$strTO = "\">";
+		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
+		$dblFROM = $dblFROM + strlen($SearchFROM);
+		$dblTO = strpos($Code,$strTO,$dblFROM);
+		$startPoint = $dblTO;
+		$dblTO = $dblTO - $dblFROM;
+		
+		$strResult = trim(substr($Code,$dblFROM,$dblTO));
+		if (strlen($strResult) > 0){
+			$strConverted = str_replace(",","",$strResult);
+			$Mendeley_Readers = $strConverted;
+		}
+		
+		$SearchFROM = "Views</span></h3></header><data class=\"number\" value=\"";
+		$strTO = "\">";
+		$dblFROM = strpos($Code,$SearchFROM,$startPoint);
+		$dblFROM = $dblFROM + strlen($SearchFROM);
+		$dblTO = strpos($Code,$strTO,$dblFROM);
+		$startPoint = $dblTO;
+		$dblTO = $dblTO - $dblFROM;
+		
+		$strResult = trim(substr($Code,$dblFROM,$dblTO));
+		if (strlen($strResult) > 0){
+			$strConverted = str_replace(",","",$strResult);
+			$Mendeley_Views = $strConverted;
+		}
+		
+	}	
 	function PrefixCheck(){
 		global $prefix;
 		if (strlen($prefix) > 1 && strlen($prefix) <= 10){
@@ -342,8 +443,9 @@
 	function ScriptAll(){
 		
 		global $GScholar;
-		global $SScopusID;
+		global $ScopusID;
 		global $ReID;
+		global $MendeleyID;		
 		global $prefix;
 		
 		global $GURL;
@@ -351,11 +453,11 @@
 		global $GhIndex;
 		global $Gi10Index;
 		
-		global $SURL;
-		global $SDocuments;
-		global $ShIndex;
-		global $SCoAuthors;
-		global $SReferences;
+		global $ScopusURL;
+		global $Scopus_hIndex;
+		global $Scopus_Documents;
+		global $Scopus_CitationDocuments;
+		global $Scopus_CoAuthors;
 		
 		global $ReIDURL;
 		global $ResearcherID_totalArticleCount;
@@ -365,6 +467,13 @@
 		global $ResearcherID_hindex;
 		global $ResearcherID_lastUpdatedString;
 		
+		global $MendeleyURL;
+		global $Mendeley_Media;
+		global $Mendeley_hIndex;
+		global $Mendeley_Citations;
+		global $Mendeley_Readers;
+		global $Mendeley_Views;
+
 		global $prefix;
 		global $EduStatsVersion;
 		
@@ -378,13 +487,13 @@
 			echo(PrefixCheck() . "Google_i10Index = " . $Gi10Index . ";");
 		}
 		
-		if (strlen($SScopusID) > 0){
-			echo(PrefixCheck() . "Scopus_ID = \"" . $SScopusID . "\";");
-			echo(PrefixCheck() . "Scopus_URL = \"" . $SURL . "\";");
-			echo(PrefixCheck() . "Scopus_Documents = " . $SDocuments . ";");
-			echo(PrefixCheck() . "Scopus_hIndex = " . $ShIndex . ";");
-			echo(PrefixCheck() . "Scopus_CoAuthors = " . $SCoAuthors . ";");
-			echo(PrefixCheck() . "Scopus_References = " . $SReferences . ";");
+		if (strlen($ScopusID) > 0){
+			echo(PrefixCheck() . "Scopus_ID = \"" . $ScopusID . "\";");
+			echo(PrefixCheck() . "Scopus_URL = \"" . $ScopusURL . "\";");
+			echo(PrefixCheck() . "Scopus_hIndex = " . $Scopus_hIndex . ";");
+			echo(PrefixCheck() . "Scopus_Documents = " . $Scopus_Documents . ";");
+			echo(PrefixCheck() . "Scopus_CitationDocuments = " . $Scopus_CitationDocuments . ";");
+			echo(PrefixCheck() . "Scopus_CoAuthors = " . $Scopus_CoAuthors . ";");
 		}
 		
 		if (strlen($ReID) > 0){
@@ -398,12 +507,22 @@
 			echo(PrefixCheck() . "ResearcherID_lastUpdatedString = \"" . $ResearcherID_lastUpdatedString . "\";");
 		}
 		
+		if (strlen($MendeleyID) > 0){
+			echo(PrefixCheck() . "Mendeley_ID = \"" . $MendeleyID . "\";");
+			echo(PrefixCheck() . "Mendeley_URL = \"" . $MendeleyURL . "\";");
+			echo(PrefixCheck() . "Mendeley_Media = \"" . $Mendeley_Media . "\";");
+			echo(PrefixCheck() . "Mendeley_hIndex = \"" . $Mendeley_hIndex . "\";");
+			echo(PrefixCheck() . "ResearcherID_Citations = \"" . $Mendeley_Citations . "\";");
+			echo(PrefixCheck() . "Mendeley_Readers = \"" . $Mendeley_Readers . "\";");
+			echo(PrefixCheck() . "Mendeley_Views = \"" . $Mendeley_Views . "\";");
+		}
 	}
 	
 	function JSONAll(){
 		global $GScholar;
-		global $SScopusID;
+		global $ScopusID;
 		global $ReID;
+		global $MendeleyID;
 		global $prefix;
 		
 		global $GURL;
@@ -411,11 +530,11 @@
 		global $GhIndex;
 		global $Gi10Index;
 		
-		global $SURL;
-		global $SDocuments;
-		global $ShIndex;
-		global $SCoAuthors;
-		global $SReferences;
+		global $ScopusURL;
+		global $Scopus_hIndex;
+		global $Scopus_Documents;
+		global $Scopus_CitationDocuments;
+		global $Scopus_CoAuthors;
 		
 		global $ReIDURL;
 		global $ResearcherID_totalArticleCount;
@@ -425,6 +544,13 @@
 		global $ResearcherID_hindex;
 		global $ResearcherID_lastUpdatedString;
 		
+		global $MendeleyURL;
+		global $Mendeley_Media;
+		global $Mendeley_hIndex;
+		global $Mendeley_Citations;
+		global $Mendeley_Readers;
+		global $Mendeley_Views;
+
 		global $EduStatsVersion;
 		
 		$strJSON = "";
@@ -439,15 +565,15 @@
 			$strJSON .= "\t\"i10Index\":" . $Gi10Index . "}\r\n";
 		}
 		
-		if (strlen($SScopusID) > 0){
+		if (strlen($ScopusID) > 0){
 			$strJSON .= ",";
 			$strJSON .= "\"Scopus\":\r\n";
-			$strJSON .= "\t{\"ID\":\"" . $SScopusID . "\",\r\n";
-			$strJSON .= "\t\"URL\":\"" . $SURL . "\",\r\n";
-			$strJSON .= "\t\"Documents\":" . $SDocuments . ",\r\n";
-			$strJSON .= "\t\"hIndex\":" . $ShIndex . ",\r\n";
-			$strJSON .= "\t\"CoAuthors\":" . $SCoAuthors . ",\r\n";
-			$strJSON .= "\t\"References\":" . $SReferences . "}\r\n";
+			$strJSON .= "\t{\"ID\":\"" . $ScopusID . "\",\r\n";
+			$strJSON .= "\t\"URL\":\"" . $ScopusURL . "\",\r\n";
+			$strJSON .= "\t\"Documents\":" . $Scopus_Documents . ",\r\n";
+			$strJSON .= "\t\"hIndex\":" . $Scopus_hIndex . ",\r\n";
+			$strJSON .= "\t\"CoAuthors\":" . $Scopus_CoAuthors . ",\r\n";
+			$strJSON .= "\t\"CitationDocuments\":" . $Scopus_CitationDocuments . "}\r\n";
 		}
 		
 		if (strlen($ReID) > 0){
@@ -461,6 +587,18 @@
 			$strJSON .= "\t\"AverageCitations\":" . $ResearcherID_averagePerItem . ",\r\n";
 			$strJSON .= "\t\"hIndex\":" . $ResearcherID_hindex . ",\r\n";
 			$strJSON .= "\t\"lastUpdatedString\":\"" . $ResearcherID_lastUpdatedString . "\"}\r\n";
+		}
+		
+		if (strlen($MendeleyID) > 0){
+			$strJSON .= ",";
+			$strJSON .= "\"Mendeley\":\r\n";
+			$strJSON .= "\t{\"ID\":\"" . $MendeleyID . "\",\r\n";
+			$strJSON .= "\t\"URL\":\"" . $MendeleyURL . "\",\r\n";
+			$strJSON .= "\t\"Media\":" . $Mendeley_Media . ",\r\n";
+			$strJSON .= "\t\"hIndex\":" . $Mendeley_hIndex . ",\r\n";
+			$strJSON .= "\t\"Citations\":" . $Mendeley_Citations . ",\r\n";
+			$strJSON .= "\t\"Readers\":" . $Mendeley_Readers . ",\r\n";
+			$strJSON .= "\t\"Views\":" . $Mendeley_Views . "}\r\n";
 		}
 		$strJSON .= "}";		
 		echo ($strJSON);
